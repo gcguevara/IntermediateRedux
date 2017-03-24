828e236fbe093bba5f6b2ab59e9be3666757d80d
gcguevara <garrettcguevara@gmail.com>
2017-03-23 23:06:26 -0700

    Finished up project, added google maps API functionality, sparklines functionality.

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -4,7 +4,9 @@ export default function(state = [], action) {
 switch (action.type) {
   case FETCH_WEATHER:
     // don't mutate state! you should use concat because it creates a new array
-    return state.concat([action.payload.data]);
+    // return state.concat([action.payload.data]);
+    // this statement with ...state is similar to concat, but uses ES6 syntax
+    return [ action.payload.data, ...state ];
   }
   return state;
 }
